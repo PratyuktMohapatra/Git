6b4a364190b82b3a6b852b2dbaf70418ecf6a832
Pratyukt Mohapatra <pratyuktmohapatra.akrabhi@gmail.com>
2023-11-08 15:58:20 +0530

    First commit

---
+++ Javascript.js
@@ -0,0 +1,23 @@
+
+var name = prompt("What is your name?");
+
+
+if (name) {
+ 
+  alert("Hello, " + name + "! Welcome to the world of JavaScript!");
+} else {
+  
+  alert("Hello, anonymous! Welcome to the world of JavaScript!");
+}
+
+var name = prompt("What is your name?");
+
+
+if (name) {
+  
+  alert("Hello, " + name + "! Welcome to the world of JavaScript!");
+} else {
+  
+  alert("Hello, anonymous! Welcome to the world of JavaScript!");
+}
+//
