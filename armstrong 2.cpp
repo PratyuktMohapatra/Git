e909a1d7a6162e30b03b9d5ba741d7ce61ba1e3a
Pratyukt Mohapatra <pratyuktmohapatra.akrabhi@gmail.com>
2023-11-08 15:19:31 +0530

    armstro

---
+++ armstrong 2.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int num,r,n,arm=0;
+	printf("enter a number");
+	scanf("%d",&num);
+	n=num;
+	while(n!=0)
+	{
+		r=n%10;
+		arm+=r*r*r;
+		n=n/10;
+	}
+	if(arm==num)
+	printf("it is a armstrong number");
+	else
+	printf("it is not an armstrong numebr");
+	return 0;
+}
